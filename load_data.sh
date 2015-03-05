c17b4c43284cc0cadcad993107d424edcbf6cf63
bryan kennedy <bkennedy@smm.org>
2015-03-05 15:19:26 -0600

    Execute as a single step to preserve directory

--- load_data.sh
+++ load_data.sh
@@ -2,5 +2,4 @@
 
 #
 # Load Mongo data
-cd ~/Desktop/source/
-mongorestore -h 127.0.0.1 --port 3001 -d meteor dump/
+cd ~/Desktop/source/ && { mongorestore -h 127.0.0.1 --port 3001 -d meteor dump/; }
