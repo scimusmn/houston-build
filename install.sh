f3527fda4da9f10d7ed175a34d22f8073bbf9b3b
bryan kennedy <bkennedy@smm.org>
2015-03-07 11:47:22 -0600

    Adding a user visible Chrome message

--- install.sh
+++ install.sh
@@ -62,6 +62,7 @@ fi
 # kiosked Chrome window (run by Stele) has focus once it is launched.
 
 # Set Chrome's presentation mode to 3, which blocks the dock and Apple menu bar
+echo "Modifying Chrome to truely go fullscreen"
 perl -i.bak -0pe 's/(NSAppleScriptEnabled.*\n(.*)<true\/>\n)/$1$2<key>LSUIPresentationMode<\/key>\n$2<integer>3<\/integer>\n/g;' /Applications/Google\ Chrome.app/Contents/Info.plist
 
 # Ensure that Stele has focus after launching with a sleep and click script
